c30cb8d152cb4e0283672f4302ef29a1fefcea79
Domye <a1523610551@163.com>
2025-08-09 22:02:52 +0800

    初步完成左侧描述

--- src/components/LeftLogo.vue
+++ src/components/LeftLogo.vue
@@ -1,7 +1,38 @@
-<template></template>
+<template>
+	<a
+		href="https://my.domye.top"
+		class="logo"
+		style="
+			background-image: url(https://q1.qlogo.cn/g?b=qq&nk=1523610551&s=640);
+		"
+	></a>
+</template>
 
 <script>
 	export default {};
 </script>
 
-<style></style>
+<style>
+	.container-left {
+		overflow-y: scroll;
+		width: 230px;
+		height: 100vh;
+		display: flex;
+		padding: 0 15px;
+		position: fixed;
+		align-items: center;
+		flex-direction: column;
+	}
+	.container-left::-webkit-scrollbar {
+		display: none;
+	}
+	.logo {
+		flex-shrink: 0;
+		width: 65%;
+		position: relative;
+		aspect-ratio: 1/1;
+		margin-top: 50px;
+		background-size: cover;
+		border-radius: 8%;
+	}
+</style>
